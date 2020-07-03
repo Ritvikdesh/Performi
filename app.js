7c19d091ec337632b9291cdcfd8f52f816a0397a
Ritvikdesh <ritvik.desh@gmail.com>
2020-07-03 12:54:07 -0400

    finsished ui design of login page

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-var express = require("express"),
-        app = express()
+var express     = require("express"),
+    mongoose    = require("mongoose"),
+    bodyParser  = require("body-parser"),
+        app     = express()
 
-// landing page
+// setting up defaults that app requires
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+// login page
 app.get("/", function(req, res){
-    res.send("hey this is the home page");
+    res.render("login");
+})
+
+//home page
+app.get("/home", function(req, res){
+    res.render("home");
+})
+
+//forgot password page
+app.get("/forgotPassword", function(req,res){
+    res.render("forgotPassword");
+})
+
+//demo login page
+app.get("/demo", function(req, res){
+    res.render("demo");
 })
 
 // running app at local host 3000
